b40c02349855489bbe7fe0ead286f948f3a1dbb2
Vladislav Atanasov <38402068+wrathfulfakey@users.noreply.github.com>
2020-04-22 17:32:12 +0300

    Update GlobalConstants.cs
    Updated for SendGrid.

--- CinemaHour.Common/GlobalConstants.cs
+++ CinemaHour.Common/GlobalConstants.cs
@@ -5,7 +5,5 @@
         public const string SystemName = "CinemaHour";
 
         public const string AdministratorRoleName = "Administrator";
-
-        public const string SendGridApiKey = "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";
     }
 }
